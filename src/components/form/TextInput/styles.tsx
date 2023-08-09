aa52baa922df120b426d3f091d89c2747f8a532a
Daniel mora <danielmora@habi.co>
2023-08-09 18:31:39 -0500

    add icon and change input text

--- src/components/form/TextInput/styles.tsx
+++ src/components/form/TextInput/styles.tsx
@@ -17,17 +17,17 @@ export const TextInputWrapper = styled.div<TextInputWrapperProps>`
     border: 1px solid
       ${({ theme, error }) => (error ? theme.red500 : theme.coolGray300)};
     padding: 12px;
-    border-radius: 18px;
+    border-radius: 6px;
     width: 100%;
     box-sizing: border-box;
     color: ${({ theme, error }) => (error ? theme.red500 : theme.coolGray700)};
-    font: 400 16px Poppins Regular;
+    font: 400 12px Poppins Regular;
     background-color: ${({ theme }) => theme.coolGray200};
-    height: 64px;
+    height: 38px;
     &:focus {
       box-shadow: 0 0
         ${({ theme, error }) =>
-          error ? `5px ${theme.red500}` : `2px ${theme.coolGray300}`};
+          error ? `5px ${theme.red500}` : `8px ${theme.coolGray300}`};
       border-color: ${({ theme, error }) =>
         error ? theme.red500 : theme.coolGray300};
       outline: none;
